fc7e3d1823dc816a828950686df52e3c036ecd46
Venkitasamy2 <DVenkitasam2@ad.harman.com>
2021-03-18 09:39:56 +0530

    initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,18 @@ import {Component} from '@angular/core';
     <img [src]="image">
   `
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public message = `Angular Universal`;
   // tslint:disable-next-line:max-line-length
   public image = 'https://camo.githubusercontent.com/81f72f2fdf98aa1d30b5b215bc8ca9420b249e81/68747470733a2f2f616e67756c61722e696f2f67656e6572617465642f696d616765732f6d61726b6574696e672f636f6e636570742d69636f6e732f756e6976657273616c2e706e67';
-
-  constructor() {}
+  title = 'Angular 10 Universal Example';
+  constructor(private titleService: Title, private metaService: Meta) {}
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Angular, Universal, Example'},
+      {name: 'description', content: 'Angular Universal Example'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+  }
 }
