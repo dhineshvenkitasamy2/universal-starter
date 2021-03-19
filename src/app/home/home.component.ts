6914e0333b3d7f5b210831427d43f717287b2eb2
Venkitasamy2 <DVenkitasam2@ad.harman.com>
2021-03-19 10:16:42 +0530

    meta tag aded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { Meta, Title } from '@angular/platform-browser';
   `
 })
 export class HomeComponent implements OnInit {
-  public message = `Angular Universal`;
+  public message = `Angular Universal test`;
   // tslint:disable-next-line:max-line-length
   public image = 'https://camo.githubusercontent.com/81f72f2fdf98aa1d30b5b215bc8ca9420b249e81/68747470733a2f2f616e67756c61722e696f2f67656e6572617465642f696d616765732f6d61726b6574696e672f636f6e636570742d69636f6e732f756e6976657273616c2e706e67';
   title = 'Angular 10 Universal Example';
@@ -21,5 +21,19 @@ export class HomeComponent implements OnInit {
       {name: 'description', content: 'Angular Universal Example'},
       {name: 'robots', content: 'index, follow'}
     ]);
+    this.metaService.addTags([
+      { name: 'og:url', content: 'https://www.nfp.com/' },
+      { name: 'og:title', content: 'TITLE CHECK' },
+      { name: 'og:description', content: 'DESCRIPTION CHECK' },
+      { name: 'og:image', content: 'https://picsum.photos/200' },
+      { name: 'og:image:width', content: "400"},
+      { name: 'og:image:height', content: "300" },
+      { name: 'twitter:title', content: 'TWITTER TITLE' },
+      { name: 'twitter:description', content: 'TWITTER DESCRIPTION' },
+      { name: 'twitter:image', content: 'https://picsum.photos/200' },
+      { name: 'twitter:image:height', content: "400"  },
+      { name: 'twitter:image:width', content: "300" },
+      { name: 'twitter:card', content: "summary" }]);
   }
+  
 }
