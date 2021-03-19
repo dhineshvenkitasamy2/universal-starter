e4cd1d2eb77de16c5ddd8dfbd21efaa04b017ff5
Venkitasamy2 <DVenkitasam2@ad.harman.com>
2021-03-19 10:33:11 +0530

    meta tag aded in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -39,6 +40,32 @@ import {Component} from '@angular/core';
     }
   `]
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  public message = `Angular Universal test`;
+  // tslint:disable-next-line:max-line-length
+  public image = 'https://camo.githubusercontent.com/81f72f2fdf98aa1d30b5b215bc8ca9420b249e81/68747470733a2f2f616e67756c61722e696f2f67656e6572617465642f696d616765732f6d61726b6574696e672f636f6e636570742d69636f6e732f756e6976657273616c2e706e67';
+  title = 'Angular 10 Universal Example';
+  constructor(private titleService: Title, private metaService: Meta) {}
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Angular, Universal, Example'},
+      {name: 'description', content: 'Angular Universal Example'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+    this.metaService.addTags([
+      { name: 'og:url', content: 'https://medium.com/google-cloud/deploying-an-angular-app-using-google-cloud-run-7a4d59048edd' },
+      { name: 'og:title', content: 'initial page title' },
+      { name: 'og:description', content: 'INITIAL PAGE DESCRIPTION CHECK' },
+      { name: 'og:image', content: 'https://i.picsum.photos/id/476/200/300.jpg?hmac=_vE--dw3keZ1r73AbtN9I362ItgpZJkRbRrJsB688Kw' },
+      { name: 'og:image:width', content: "400"},
+      { name: 'og:image:height', content: "300" },
+      { name: 'twitter:title', content: 'INITIAL PAGE TWITTER TITLE' },
+      { name: 'twitter:description', content: 'INITIAL PAGE TWITTER DESCRIPTION' },
+      { name: 'twitter:image', content: 'https://i.picsum.photos/id/476/200/300.jpg?hmac=_vE--dw3keZ1r73AbtN9I362ItgpZJkRbRrJsB688Kw' },
+      { name: 'twitter:image:height', content: "400"  },
+      { name: 'twitter:image:width', content: "300" },
+      { name: 'twitter:card', content: "summary" }]);
+  }
+  
 }
